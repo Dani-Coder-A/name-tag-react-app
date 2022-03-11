28d3df775d29ad78202d8c2c1acf31da9936ea18
Dani A <danielle.arlinghaus25@gmail.com>
2022-03-10 19:48:41 -0500

    Updates

---
+++ precache-manifest.6856891481d099f74934a278ee2af5b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06188db56e974673afc0",
+    "url": "/name-tag-react-app/static/css/main.9aebe120.chunk.css"
+  },
+  {
+    "revision": "06188db56e974673afc0",
+    "url": "/name-tag-react-app/static/js/main.bab6682b.chunk.js"
+  },
+  {
+    "revision": "952ee552305a038abbc2",
+    "url": "/name-tag-react-app/static/js/runtime~main.d636409a.js"
+  },
+  {
+    "revision": "45a7acfdace2e315d2d9",
+    "url": "/name-tag-react-app/static/js/2.ee49d4bf.chunk.js"
+  },
+  {
+    "revision": "920e2b8bf0e8625fd517247abf24a718",
+    "url": "/name-tag-react-app/index.html"
+  }
+];
